1966133667f9220c027ee2d546aca104cb713064
Daniel Grace <negativeview@gmail.com>
2016-07-11 22:40:40 +0000

    Move toward a more broken up layout.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 "use strict";
 
 const RollInput = require('./roll-input.js');
+const RollParser = require('./roll-parser.js');
 
 module.exports = {
-	RollInput: RollInput
+	RollInput: RollInput,
+	RollParser: RollParser
 };
