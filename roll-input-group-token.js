55ce1d4e1df2cf26b753c2fb89ba5c215a017ad3
Daniel Grace <negativeview@gmail.com>
2016-07-11 22:08:19 +0000

    Number, not lexeme.

--- roll-input-group-token.js
+++ roll-input-group-token.js
@@ -45,8 +45,7 @@ class RollInputGroupToken extends RollInputToken {
 					this.operation = 'full-group';
 
 					if (index != 0 && arr[index - 1].type == 'number') {
-						console.log('pulling repeat from', arr[index - 1]);
-						this.repeat = arr[index - 1].lexeme;
+						this.repeat = arr[index - 1].number;
 						arr.splice(index - 1, 1);
 					} else {
 						this.repeat = 1;
