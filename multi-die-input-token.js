09107c97dfa582fa4d4aae6e0a113853af022644
Daniel Grace <negativeview@gmail.com>
2016-07-12 00:38:55 +0000

    Oops.

--- multi-die-input-token.js
+++ multi-die-input-token.js
@@ -17,7 +17,7 @@ class MultiDieInputToken extends NumericInputToken {
 		var res = this.dieNumber + 'd' + this.dieSize + ' (';
 		res += this.result.getResults().map(
 			(currentValue, index, array) => {
-				return currentValue[0];
+				return currentValue.getResults()[0];
 			}
 		).join(', ');
 		res += ')';
